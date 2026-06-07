f86ed598bad094ab678b0012994fd07c3613fb70
ptejasv <e0564194@u.nus.edu>
2026-06-07 16:40:48 +0800

    feat: add basic etch-a-sketch functionality

---
+++ scripts/etch_a_sketch.js
@@ -0,0 +1,48 @@
+let INITIAL_SIDES = 16;
+
+const containerDiv = document.querySelector("#container");
+
+function setupGrid(numSides) {
+    // setup the grid
+    for (let i = 0; i < numSides * numSides; i++) {
+        let gridSquare = document.createElement("div");
+        gridSquare.classList.add("grid-square");
+
+        // calculate the height and width of each square
+        // use the window height as the side for the square container
+        containerDiv.style["width"] = containerDiv.style["height"] = `${window.innerHeight - 60}px`;
+        gridSquare.style["height"] = gridSquare.style["flex-basis"] = `${100 / numSides}%`;
+        containerDiv.appendChild(gridSquare);
+    }
+}
+
+function setupSquares() {
+    // add handler for hovering on each square
+    let gridSquares = document.querySelectorAll(".grid-square");
+    gridSquares.forEach((square) => {
+        square.addEventListener("mouseenter", (e) => {
+            e.target.classList.add("hovered");
+        })
+    });
+}
+
+// add handler to "recreate sketchpad" button
+const recreateButton = document.querySelector("button");
+recreateButton.addEventListener("click", () => {
+    let numSides = parseInt(prompt("How many sides would you like the new sketchpad to have?"));
+
+    // check input validity
+    if (numSides > 100) alert("Please use a value less than or equal to 100.");
+    else if (numSides < 1) alert("Please use a value above 0.");
+    else if (isNaN(numSides)) alert("Please enter a number.");
+    else {
+        // clear exisitng sketchpad and setup again
+        containerDiv.innerHTML = "";
+        setupGrid(numSides);
+        setupSquares();
+    };
+});
+
+// initial grid setup
+setupGrid(INITIAL_SIDES);
+setupSquares();
